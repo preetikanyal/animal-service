5f6439131ea5381b2990816c42cc3f8e959540f1
preetikanyal <preeti.kanyal02@gmail.com>
2021-11-10 23:46:55 +0530

    Update ValidationHandler.java

--- src/main/java/com/preeti/animals/validation/ValidationHandler.java
+++ src/main/java/com/preeti/animals/validation/ValidationHandler.java
@@ -1,35 +1,84 @@
 package com.preeti.animals.validation;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.http.HttpHeaders;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * This class computes validation message to be displayed to the user
  * 
  */
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
-public class ValidationHandler extends ResponseEntityExceptionHandler {
+public class ValidationHandler{
 
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
+	/*
+	 * @Override protected ResponseEntity<Object>
+	 * handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders
+	 * headers, HttpStatus status, WebRequest request) {
+	 * 
+	 * Map<String, String> errors = new HashMap<>();
+	 * ex.getBindingResult().getAllErrors().forEach((error) -> {
+	 * 
+	 * String fieldName = ((FieldError) error).getField(); String message =
+	 * error.getDefaultMessage(); errors.put(fieldName, message); }); return new
+	 * ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST); }
+	 */
+	@ResponseStatus(BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Error methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<org.springframework.validation.FieldError> fieldErrors = result.getFieldErrors();
+        return processFieldErrors(fieldErrors);
+    }
 
-		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
+    private Error processFieldErrors(List<org.springframework.validation.FieldError> fieldErrors) {
+        Error error = new Error(HttpStatus.BAD_REQUEST.value(), "validation error");
+        for (org.springframework.validation.FieldError fieldError: fieldErrors) {
+            error.addFieldError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return error;
+    }
 
-			String fieldName = ((FieldError) error).getField();
-			String message = error.getDefaultMessage();
-			errors.put(fieldName, message);
-		});
-		return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
-	}
+    static class Error {
+        private final int status;
+        private final String message;
+        private List<FieldError> fieldErrors = new ArrayList<>();
+
+        Error(int status, String message) {
+            this.status = status;
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void addFieldError(String path, String message) {
+            FieldError error = new FieldError(path, message,"");
+            fieldErrors.add(error);
+        }
+
+        public List<FieldError> getFieldErrors() {
+            return fieldErrors;
+        }
+    }	
 }
